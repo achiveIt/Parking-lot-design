db0167206d9dedde35dc225ded5f8f7a0dab98fa
achiveIt <154082077+achiveIt@users.noreply.github.com>
2025-02-18 00:34:36 +0530

    fix: update constants

--- src/com/demo/Constants.java
+++ src/com/demo/Constants.java
@@ -1,6 +1,7 @@
 package com.demo;
 
 public class Constants {
-    public static final int CAR_BIKE_SPOTS = 1;
+    public static final int CAR_SPOTS = 1;
+    public static final int BIKE_SPOTS = 1;
     public static final int TRUCK_SPOTS = 2;
 }
