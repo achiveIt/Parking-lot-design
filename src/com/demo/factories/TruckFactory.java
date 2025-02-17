83e4631ba67a33b85d1aef107eee87a39e2e0205
achiveIt <154082077+achiveIt@users.noreply.github.com>
2025-02-17 17:45:11 +0530

    refactor: refactor vehicle factory to an interface

---
+++ src/com/demo/factories/TruckFactory.java
@@ -0,0 +1,14 @@
+package com.demo.factories;
+
+import com.demo.interfaces.VehicleService;
+
+public class Truck implements vehicleFactory{
+
+    private String vehicleNumber;
+
+    public Truck
+    @Override
+    public VehicleService createVehicle() {
+        return null;
+    }
+}
---
+++ src/com/demo/factories/vehicleFactory.java
@@ -0,0 +1,7 @@
+package com.demo.factories;
+
+import com.demo.interfaces.VehicleService;
+
+public interface vehicleFactory {
+    VehicleService createVehicle();
+}
