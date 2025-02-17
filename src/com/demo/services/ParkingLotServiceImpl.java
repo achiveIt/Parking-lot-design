ffaf961c81f24fc744b15fe87be0f8573bfd12f2
achiveIt <happyface979797@gmail.com>
2025-02-17 10:56:26 +0530

    feat: add parking lot service implementation:

---
+++ src/com/demo/services/ParkingLotServiceImpl.java
@@ -0,0 +1,85 @@
+package com.demo.services;
+
+import com.demo.enums.VehicleType;
+import com.demo.interfaces.FloorService;
+import com.demo.interfaces.ParkingLotService;
+import com.demo.interfaces.VehicleService;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class ParkingLotServiceImpl implements ParkingLotService {
+    private List<FloorService> floors;
+    private Map<String, Integer> vehicleLocation;
+
+    public ParkingLotServiceImpl(int numOfFloors, int numOfSpotsPerFloor) {
+        this.floors = new ArrayList<>();
+        this.vehicleLocation = new HashMap<>();
+
+        for(int i = 0; i < numOfFloors; i++){
+            floors.add(new FloorServiceImpl(i + 1, numOfSpotsPerFloor));
+        }
+    }
+
+
+    @Override
+    public boolean parkVehicle(String vehicleNumber, VehicleType vehicleType) {
+        VehicleService vehicle = VehicleFactory.createVehicle(vehicleNumber, vehicleType);
+
+        if(vehicle == null) return false;
+
+        for(FloorService floor : floors){
+            if(floor.parkVehicle(vehicle)){
+                vehicleLocation.put(vehicleNumber, floor.getFloorNumber());
+                return true;
+            }
+        }
+        return false;
+    }
+
+    @Override
+    public boolean removeVehicle(String vehicleNumber) {
+        Integer floorNumber = vehicleLocation.get(vehicleNumber);
+        if(floorNumber == null) return false;
+
+        FloorService floor = floors.get(floorNumber - 1);
+        boolean removed = floor.removeVehicle(vehicleNumber);
+
+        if(removed) vehicleLocation.remove(vehicleNumber);
+        return removed;
+    }
+
+    @Override
+    public int availableSpots(int floor) {
+        if(floor< 1 || floor>floors.size()){
+            return -1;
+        }
+
+        return floors.get(floor - 1).availableSpots();
+    }
+
+    @Override
+    public boolean isFull() {
+        int noOfSpot = 0;
+        for(FloorService floor: floors){
+            noOfSpot += floor.availableSpots();
+        }
+        return noOfSpot == 0 ?  true : false;
+    }
+
+    @Override
+    public String getVehicleLocation(String vehicleNumber) {
+        Integer floorNum = vehicleLocation.get(vehicleNumber);
+
+        if(floorNum == null){
+            return "Sorry this vehicle is not parked in our parking lot";
+        }
+
+        FloorService floor = floors.get(floorNum - 1);
+        Integer spotNum = floor.getVehicleSpot(vehicleNumber);
+
+        return "Floor: " + floorNum + ", Spot: " + spotNum;
+    }
+}
